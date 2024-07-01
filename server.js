3327aa8f7f3e355f40c3e54dd1c882bec758a819
Parasite-231 <muktadir@iut-dhaka.edu>
2024-07-01 22:58:19 +0600

    check5x

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
-
 const app = express();
 
 app.get('/', (req, res) => {
   const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, html) => {
-    if (err) {
-      res.status(500).send("Error reading HTML file");
-      return;
-    }
-    const updatedHtml = html.replace('<%= ipAddress %>', ipAddress);
-    console.log(ipAddress)
-    res.send(updatedHtml);
-  });
+  console.log("User IP Address:", ipAddress);
+  res.send(ipAddress);
 });
 
 app.listen(process.env.PORT || 3000, () => {
